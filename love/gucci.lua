7c3d5c266cf641096ef0af2b33b4a7c244f81699
Thetan <83221369+Thetan-ILW@users.noreply.github.com>
2025-04-14 12:01:54 +0300

    Update file list

--- love/gucci.lua
+++ love/gucci.lua
@@ -2,7 +2,7 @@ local gucci = {}
 
 function gucci.setDefaultSettings(configs)
 	local osu = configs.osu_ui
-	osu.scoreSystem = "osu!legacy"
+	osu.scoreSystem = "osuLegacy"
 	osu.judgement = 8
 	osu.songSelect.scoreSource = "osuv1"
 end
